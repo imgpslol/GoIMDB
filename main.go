0c1b31b92c9ed144bdf354d2bf2750fd92489c9e
imgpslol <garyenots@gmail.com>
2026-08-22 19:28:12 +0100

    added basic webserver

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World")
+
+	h1 := func (w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, World! \n")
+		io.WriteString(w, r.Method)
+	}
+
+	http.HandleFunc("/", h1)
 	//func ListenAndServe(addr string, handler Handler) error
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
