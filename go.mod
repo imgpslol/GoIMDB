d60b33cf0b3ddc5dd62c806d6070693ffb3fee7c
imgpslol <garyenots@gmail.com>
2026-08-22 19:09:54 +0100

    setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoIMDB
+
+go 1.26.6
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	//func ListenAndServe(addr string, handler Handler) error
+}
