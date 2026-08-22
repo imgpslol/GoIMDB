db03768b10c6932966098c5875dce4e86b8a21a4
imgpslol <garyenots@gmail.com>
2026-08-22 20:32:58 +0100

    setup manual movies and grabbed template from main.go to display in index.html

--- main.go
+++ main.go
@@ -7,12 +7,27 @@ import (
 	"net/http"
 )
 
+// Tempoarily building movie list manually will come back to this later and build a database and connect to IMDB API to get movie list and details, tags and reviews.
+type Movie struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	fmt.Println("Hello, World")
 
-	h1 := func (w http.ResponseWriter, r *http.Request) {
+	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+
+		// Remove later temporarily building movie list manually will come back to this later and build a database and connect to IMDB API to get movie list and details, tags and reviews.
+		movies := map[string][]Movie{
+			"Movies": {
+				{Title: "Inception", Director: "Christopher Nolan"},
+				{Title: "The Matrix", Director: "Lana Wachowski, Lilly Wachowski"},
+				{Title: "The Shawshank Redemption", Director: "Frank Darabont"},
+			},
+		}
+		tmpl.Execute(w, movies)
 	}
 
 	http.HandleFunc("/", h1)
